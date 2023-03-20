d06f9106fee5db8c2ffe432a0e331068a75dda22
Timo Ruohomaki <timo@ruohomaki.fi>
2023-03-20 13:58:09 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,24 @@
+
 ## This function creates a special matrix that can cache its inverse
 
-
-
 makeCacheMatrix <- function(x = matrix()) {
+  
+    mCache <- NULL # creating empty cache object for inversed matrix
 
+    setMatrix <- function(y) {
+        x <<- y
+        mCache <<- NULL
+  }
+  
+    getMatrix <- function() x
+    setInverse <- function(inverse) mCache <<- inverse
+    getInverse <- function() mCache
+    
+    list( setMatrix = setMatrix,
+          getMatrix = getMatrix, 
+          setInverse = setInverse,
+          getInverse = getInverse)
+  
 }
 
 
@@ -13,5 +28,40 @@ makeCacheMatrix <- function(x = matrix()) {
 ## inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  mCache <- x$getInverse()
+  
+  if (!is.null(mCache)) {
+    # cache exists, returning it instead
+    message("Returning cached value")
+    return(mCache)
+  }
+  
+  data <- x$getMatrix()
+  
+  # Fun fact: matrix is invertible if det(data) != 0
+  
+  if(det(data) != 0) {
+    message("Matrix is invertible")
+  }
+  
+  mCache <- solve(data, ...)
+  x$setInverse(mCache)
+  
+  message("Returning new inversion")
+  return(mCache)
+  
 }
+
+# === UNIT TEST ====
+
+message(" Running unit tests:")
+
+m1 <- matrix(c(12,19,32,42,49,56,65,72,99),3,3)
+
+message("Original matrix:")
+m1
+
+i1 <- makeCacheMatrix(m1)
+cacheSolve(i1)
+cacheSolve(i1)
